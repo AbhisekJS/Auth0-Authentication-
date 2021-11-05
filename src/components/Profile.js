0124265642600d19339bd0d844d381c7eb01211b
AbhisekJS <abhisek.khuntia16@gmail.com>
2021-11-05 15:34:30 +0530

    initial commit

---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import {useAuth0} from '@auth0/auth0-react'
+import JSONPretty from 'react-json-pretty'
+const Profile = () => {
+    const {user,isAuthenticated}= useAuth0();
+    if(isAuthenticated){
+       localStorage.setItem('Login User',JSON.stringify({user}))
+    }
+    const[data,setData]=useState(JSON.parse( localStorage.getItem('Login User')))
+    console.log(data,'data',isAuthenticated)
+    return (
+        data && (<div>
+            <img src={data?.user?.picture} alt=""/>
+            <h2>{data?.user?.name}</h2>
+            <p>{data?.user?.email}</p>
+            <JSONPretty data={user}/>
+        </div>)
+    )
+}
+
+export default Profile
